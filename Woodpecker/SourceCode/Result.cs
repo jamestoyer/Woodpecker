4f1f9a361a7ccf2ccd243b7de9f2e7731c0f026c
James Toyer <jamestoyer@toyertechnologies.com>
2011-11-27 14:47:57 +0000

    Source code result abstract class implemented

---
+++ Woodpecker/SourceCode/Result.cs
@@ -0,0 +1,23 @@
+﻿namespace Toyertechnologies.Woodpecker.SourceCode
+{
+   /// <summary>
+   /// An abstract representation of a source code result
+   /// </summary>
+   public abstract class Result
+   {
+       /// <summary>
+       /// Gets or sets a value indicating whether the success or the source code retrieval
+       /// </summary>
+       public bool IsSuccess { get; set; }
+
+       /// <summary>
+       /// Gets or sets the local output location source location
+       /// </summary>
+       public string OutputLocation { get; set; }
+
+       /// <summary>
+       /// Gets or sets the messages output from the source code provider
+       /// </summary>
+       public string Messages { get; set; }
+   }
+}
