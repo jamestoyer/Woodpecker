6dcc038504870e2dc5cf75f2a0890bd777eedb76
James Toyer <jamestoyer@toyertechnologies.com>
2011-12-29 20:51:47 +0000

    Git source code parameters class implemented
    BugzID: 52

---
+++ Woodpecker/Git/Parameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Toyertechnologies.Woodpecker.Git
+{
+    /// <summary>
+    /// Parameters required for executing a Git action
+    /// </summary>
+    public class Parameters : SourceCode.Parameters
+    {
+    }
+}
